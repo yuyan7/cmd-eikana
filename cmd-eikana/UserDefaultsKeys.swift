988d3969ea3ab99874adf556930b9c5d86039691
Yuya Nakamura <yuyan7sh@gmail.com>
2026-03-07 02:54:11 +0900

    refactor: UserDefaultsキーを定数化
    - UserDefaultsKey enumでキーを一元管理
    - タイポ防止と保守性向上

---
+++ cmd-eikana/UserDefaultsKeys.swift
@@ -0,0 +1,18 @@
+//
+//  UserDefaultsKeys.swift
+//  ⌘英かな
+//
+//  MIT License
+//  Copyright (c) 2016 iMasanari
+//
+
+import Cocoa
+
+enum UserDefaultsKey {
+    static let launchAtStartup = "launchAtStartup"
+    static let checkUpdateAtLaunch = "checkUpdateAtLaunch"
+    static let showIcon = "showIcon"
+    static let exclusionApps = "exclusionApps"
+    static let mappings = "mappings"
+    static let oneShotModifiers = "oneShotModifiers"
+}
