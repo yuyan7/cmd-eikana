c732e946b878107605a4ba34979d2d05adc63a10
Yuya Nakamura <yuyan7sh@gmail.com>
2026-03-09 01:55:48 +0900

    fix: アプリ起動時にクラッシュする問題を解消

--- cmd-eikana/checkUpdate.swift
+++ cmd-eikana/checkUpdate.swift
@@ -41,16 +41,18 @@ func checkUpdate(_ callback: ((_ isNewVer: Bool?) -> Void)? = nil) {
         let isAbleUpdate: Bool? = (newVersion == "") ? nil : newVersion != version
         
         if isAbleUpdate == true {
-            let alert = NSAlert()
-            alert.messageText = "⌘英かな ver.\(newVersion) が利用可能です"
-            alert.informativeText = description
-            alert.addButton(withTitle: "Download")
-            alert.addButton(withTitle: "Cancel")
-            // alert.showsSuppressionButton = true;
-            let ret = alert.runModal()
-            
-            if (ret == NSApplication.ModalResponse.alertFirstButtonReturn) {
-                NSWorkspace.shared.open(URL(string: url)!)
+            DispatchQueue.main.async {
+                let alert = NSAlert()
+                alert.messageText = "⌘英かな ver.\(newVersion) が利用可能です"
+                alert.informativeText = description
+                alert.addButton(withTitle: "Download")
+                alert.addButton(withTitle: "Cancel")
+                // alert.showsSuppressionButton = true;
+                let ret = alert.runModal()
+                
+                if (ret == NSApplication.ModalResponse.alertFirstButtonReturn) {
+                    NSWorkspace.shared.open(URL(string: url)!)
+                }
             }
         }
         
