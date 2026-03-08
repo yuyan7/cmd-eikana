5a4942d6b8b6ace2cef338621283b6fcbfb186c7
Yuya Nakamura <yuyan7sh@gmail.com>
2026-03-09 00:20:32 +0900

    fix: ステータスバーのアイコンが動かない問題を修正

--- cmd-eikana/KeyEvent.swift
+++ cmd-eikana/KeyEvent.swift
@@ -256,9 +256,11 @@ class KeyEvent: NSObject {
         
         let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
         
-        CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
-        CGEvent.tapEnable(tap: eventTap, enable: true)
-        CFRunLoopRun()
+        DispatchQueue.global(qos: .userInitiated).async {
+            CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+            CGEvent.tapEnable(tap: eventTap, enable: true)
+            CFRunLoopRun()
+        }
     }
     
     func eventCallback(proxy: CGEventTapProxy, type: CGEventType, event: CGEvent) -> Unmanaged<CGEvent>? {
